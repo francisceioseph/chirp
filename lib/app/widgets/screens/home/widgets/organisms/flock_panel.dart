e4c2a2fd4b0cd4ce5e4377451d877ae4f4172e2e
Francisco Souza <francissouza1991@gmail.com>
2026-02-21 16:09:54 -0300

    feat: add nearby tiels list

--- lib/app/widgets/screens/home/widgets/organisms/flock_panel.dart
+++ lib/app/widgets/screens/home/widgets/organisms/flock_panel.dart
@@ -1,3 +1,4 @@
+import 'package:chirp/app/widgets/screens/home/widgets/organisms/nearby_tiels_list.dart';
 import 'package:flutter/material.dart';
 
 class FlockPanel extends StatelessWidget {
@@ -5,7 +6,44 @@ class FlockPanel extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    return DefaultTabController(
+      length: 2,
+      child: Column(
+        children: [
+          TabBar(
+            tabs: [
+              Tab(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.chat_bubble_outline, size: 18),
+                    SizedBox(width: 8),
+                    Text('Conversas'),
+                  ],
+                ),
+              ),
+
+              Tab(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.radar, size: 18),
+                    SizedBox(width: 8),
+                    Text('No Radar'),
+                  ],
+                ),
+              ),
+            ],
+          ),
+
+          Expanded(
+            child: TabBarView(
+              children: [const Placeholder(), const NearbyTielsList()],
+            ),
+          ),
+        ],
+      ),
+    );
 
     // TODO: RESTORE THIS PANEL LATER
     // final chirpCtrl = context.watch<ChirpController>();
